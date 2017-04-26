648d069b85a567b983761531915dce58a21d5f5a
Nathenael <tarekegnnathenael@gmail.com>
2017-04-26 22:40:00 +0300

    this is the first progress commit

---
+++ Mover/Mover/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mover.Startup))]
+namespace Mover
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
